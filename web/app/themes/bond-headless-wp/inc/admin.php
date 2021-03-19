b33f6d81b0ecb22df27805e64cc59c612128fbe1
Justin Bond <bond.justink@gmail.com>
2021-03-19 13:15:53 -0700

    redirect homepage && disable gutenberg && add project post type

--- web/app/themes/bond-headless-wp/inc/admin.php
+++ web/app/themes/bond-headless-wp/inc/admin.php
@@ -11,6 +11,11 @@
 add_theme_support( 'post-thumbnails' );
 
 /**
+ * Disable the Gutenberg WordPress Editor
+ */
+add_filter('use_block_editor_for_post', '__return_false', 10);
+
+/**
  * By default, in Add/Edit Post, WordPress moves checked categories to the top of the list and unchecked to the bottom.
  * When you have subcategories that you want to keep below their parents at all times, this makes no sense.
  * This function removes automatic reordering so the categories widget retains its order regardless of checked state.
@@ -127,6 +132,71 @@ add_filter( 'rest_prepare_page', 'set_preview_link_in_rest_response', 10, 2 );
 // }
 // add_action( 'admin_head-post-new.php', 'posttype_admin_css' );
 // add_action( 'admin_head-post.php', 'posttype_admin_css' );
+
+/**
+ * Add Custom Post Types
+ */
+function create_posttype() {
+
+  register_post_type('projects', array(
+    // This array of options controls the labels displayed in the WordPress Admin UI
+    'labels' => array(
+      'name' => __('Projects'),
+      'singular_name' => __('Project'),
+      'add_new_item' => __('Add New Project'),
+      'edit_item' => __('Edit Project')
+    ),
+    'supports' => array('title', 'thumbnail'),
+    'public' => true,
+    'has_archive' => true,
+    'rewrite' => array('slug' => 'projects'),
+    'menu_position' => 6,
+    'menu_icon' => 'dashicons-networking',
+    'show_in_rest' => true,
+    'show_in_graphql' => true,
+    'graphql_single_name' => 'project',
+    'graphql_plural_name' => 'projects',
+  ));
+}
+// Hooking up our function to theme setup
+add_action('init', 'create_posttype');
+
+/**
+ * Add Custom Taxonomies
+ */
+function add_custom_taxonomies() {
+  // Add new "Portfolio Type" taxonomy to News
+  // register_taxonomy('portfolio-types', ['portfolios'], array(
+  //   // Hierarchical taxonomy (like categories)
+  //   'hierarchical' => true,
+  //   // This array of options controls the labels displayed in the WordPress Admin UI
+  //   'labels' => array(
+  //     'name' => _x('Portfolio Types', 'taxonomy general name'),
+  //     'singular_name' => _x('Portfolio Type', 'taxonomy singular name'),
+  //     'search_items' =>  __('Search Portfolio Types'),
+  //     'all_items' => __('All Portfolio Types'),
+  //     'parent_item' => __('Parent Portfolio Type'),
+  //     'parent_item_colon' => __('Parent Portfolio Type:'),
+  //     'edit_item' => __('Edit Portfolio Type'),
+  //     'update_item' => __('Update Portfolio Type'),
+  //     'add_new_item' => __('Add New Portfolio Type'),
+  //     'new_item_name' => __('New Portfolio Type'),
+  //     'menu_name' => __('Portfolio Type'),
+  //   ),
+  //   // Control the slugs used for this taxonomy
+  //   'rewrite' => array(
+  //     'slug' => 'portfolio-types', // This controls the base slug that will display before each term
+  //     'with_front' => false, // Don't display the category base before "/portfolio-types/"
+  //     'hierarchical' => true // This will allow URL's like "/portfolio-types/boston/cambridge/"
+  //   ),
+  //   'show_in_rest' => true,
+  //   'show_in_graphql' => true,
+  //   'graphql_single_name' => 'portfolioType',
+  //   'graphql_plural_name' => 'portfolioTypes',
+  //   'show_admin_column' => true,
+  // ));
+}
+add_action('init', 'add_custom_taxonomies', 0);
 
 /**
  * Correct Permalinks - Point links inside WP Admin to the correct front-end domain
--- web/app/themes/bond-headless-wp/index.php
+++ web/app/themes/bond-headless-wp/index.php
@@ -5,8 +5,10 @@
  * @package  Bond_Headless_WP
  */
 
-// Redirect individual posts to the REST API endpoint.
-if ( is_singular() ) {
+// Redirect main URL to static site and individual posts to the REST API endpoint.
+if (is_front_page()) {
+  header( 'Location: https://justinbond.dev' );
+} else if ( is_singular() ) {
   header(
     sprintf(
       'Location: /wp-json/wp/v2/%s/%s',
